a840334f334ecc6f5e402b9e064d590ca6dd5240
RaymWong <46432420+RaymWong@users.noreply.github.com>
2022-02-28 14:34:19 +0800

    1) Add concurrent 3 servers and clients test scripts

---
+++ scripts/openssl_multisession_test/step3_start_all_clients.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+source config.sh
+#~ $LIB_FOLDER/./optiga_init_example -open
+echo "-----> Running the test client "
+#~ openssl s_client -connect 127.0.0.1:5000 -client_sigalgs ECDSA+SHA256 -keyform engine -engine trustm_engine -cert client1_e0f1.crt -key 0xe0f1:^ -tls1_2 -CAfile $CERT_PATH/OPTIGA_Trust_M_Infineon_Test_CA.pem -verify 1
+
+lxterminal -e openssl s_client -connect localhost:5002 -client_sigalgs ECDSA+SHA256 -keyform engine -engine trustm_engine -cert client1_e0f3.crt -key 0xe0f3:^ -tls1_2 -CAfile $CERT_PATH/OPTIGA_Trust_M_Infineon_Test_CA.pem 
+
+lxterminal -e  openssl s_client -connect localhost:5001 -client_sigalgs ECDSA+SHA256 -keyform engine -engine trustm_engine -cert client1_e0f2.crt -key 0xe0f2:^ -tls1_2 -CAfile $CERT_PATH/OPTIGA_Trust_M_Infineon_Test_CA.pem 
+
+lxterminal -e openssl s_client -connect localhost:5000 -client_sigalgs ECDSA+SHA256 -keyform engine -engine trustm_engine -cert client1_e0f1.crt -key 0xe0f1:^ -tls1_2 -CAfile $CERT_PATH/OPTIGA_Trust_M_Infineon_Test_CA.pem 
