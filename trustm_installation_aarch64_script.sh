3a8cfc3c44e31b439a29e93ac3c8483f701afcc0
ying-css <ying.ye@infineon.com>
2023-06-22 17:16:28 +0800

    1)Added script to install linux tools for AARCH64 RPI

---
+++ trustm_installation_aarch64_script.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+FILE="0001-trust_m_lib.patch"
+TRUSTM_LIB_PATH="${PWD}trustm_lib/"
+CURRENT_DIR="${PWD}"
+PATCH="${PWD}/${FILE}"
+
+
+sudo apt update 
+sudo apt -y install awscli git gcc libssl-dev
+
+echo $PATCH
+echo $TRUSTM_LIB_PATH
+
+echo "-----> Apply patch"
+git apply $FILE
+
+set -e
+echo "-----> Build Trust M Linux Tools for aarch64"
+sudo make uninstall AARCH64=YES
+make -j5 AARCH64=YES
+sudo make install AARCH64=YES
+
+#~ echo "-----> Build Protected Update Set tool"
+#~ cd ex_protected_update_data_set/Linux/
+#~ make clean
+#~ make -j5 AARCH64=YES
+#~ sudo make install AARCH64=YES
+ 
+#~cd $CURRENT_DIR
+echo "-----> Installation completed. Back to ${PWD}"
+
+
+
