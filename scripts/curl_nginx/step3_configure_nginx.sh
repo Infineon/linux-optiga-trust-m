40dea3008142e226abd2ac31af9b4522a38ce11f
RaymWong <46432420+RaymWong@users.noreply.github.com>
2023-07-30 09:39:59 +0800

    Update step3_configure_nginx.sh
    Restart the nginx service

--- scripts/curl_nginx/step3_configure_nginx.sh
+++ scripts/curl_nginx/step3_configure_nginx.sh
@@ -10,5 +10,5 @@ echo "Client1:-----> Configure NGINX"
 sudo cp default /etc/nginx/sites-enabled/default
 sudo cp $SERVER_CERT_NAME /etc/nginx/$SERVER_CERT_NAME
 sudo cp $SERVER_PRIVATE_KEY /etc/nginx/$SERVER_PRIVATE_KEY
-
+sudo service nginx restart
 
