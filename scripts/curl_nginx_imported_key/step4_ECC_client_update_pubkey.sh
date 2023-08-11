ced9496a38efc3d50c117374a07dc3dcb952246d
RaymWong <46432420+RaymWong@users.noreply.github.com>
2023-08-11 14:24:19 +0800

    added -e option to erase data before write for public key write operation

--- scripts/curl_nginx_imported_key/step4_ECC_client_update_pubkey.sh
+++ scripts/curl_nginx_imported_key/step4_ECC_client_update_pubkey.sh
@@ -4,4 +4,4 @@ source config.sh
 
 set -e
 echo "Client1:-----> Update Public key into Optiga Trust M"
-$EXEPATH/trustm_data -w $TRUST_M_ECC_PUBKEY_OID -i client1_pubkey.der
+$EXEPATH/trustm_data -w $TRUST_M_ECC_PUBKEY_OID -i client1_pubkey.der -e
