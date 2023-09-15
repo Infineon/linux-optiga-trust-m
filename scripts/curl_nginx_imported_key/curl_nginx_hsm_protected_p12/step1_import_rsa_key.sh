3549922d77eb9286bf1f74f53a0dafdc84ab0cfa
ying-css <ying.ye@infineon.com>
2023-09-15 22:03:44 +0800

    1)Updated script for RSA2048 Key import

--- scripts/curl_nginx_imported_key/curl_nginx_hsm_protected_p12/step1_import_rsa_key.sh
+++ scripts/curl_nginx_imported_key/curl_nginx_hsm_protected_p12/step1_import_rsa_key.sh
@@ -9,10 +9,10 @@ echo "Client1:-----> Update Public key store into Optiga Trust M"
 $EXEPATH/trustm_data -w 0x$PUBKEY_OID_RSA -i $TEMP_PUBKEY_KEY_DER_RSA -e
 
 echo "Client1:-----> Generate Manifest"
-$UPDATEPATH/trustm_protected_update_set payload_version=$PAYLOAD_VER trust_anchor_oid=$TRUST_ANCHOR target_oid=$TARGET_OID_RSA sign_algo=$SIGN_ALGO priv_key=$PRIV_KEY payload_type=key key_algo=65 key_usage=13 key_data=$TEMP_KEY_RSA secret=$SECRET label="ecc_key" seed_length=64 enc_algo="AES-CCM-16-64-128" secret_oid=$SECRET_OID
+$UPDATEPATH/trustm_protected_update_set payload_version=$PAYLOAD_VER trust_anchor_oid=$TRUST_ANCHOR target_oid=$TARGET_OID_RSA sign_algo=$SIGN_ALGO priv_key=$PRIV_KEY payload_type=key key_algo=66 key_usage=13 key_data=$TEMP_KEY_RSA secret=$SECRET label="rsa_key" seed_length=64 enc_algo="AES-CCM-16-64-128" secret_oid=$SECRET_OID
 #~ rm $TEMP_KEY
 echo "Protected RSA Key Update for 0x$TARGET_OID_RSA"
-$EXEPATH/trustm_protected_update_rsakey -k 0x$TARGET_OID_RSA -m manifest2.dat -f fragment2.dat
+$EXEPATH/trustm_protected_update_rsakey -k 0x$TARGET_OID_RSA -m manifest1.dat -f fragment1.dat
 echo "read out metadata for 0x$TARGET_OID_RSA"
 $EXEPATH/trustm_metadata -r  0x$TARGET_OID_RSA -X
 
