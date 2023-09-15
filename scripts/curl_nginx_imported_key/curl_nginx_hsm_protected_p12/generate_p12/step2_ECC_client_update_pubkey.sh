342e29ddba0d30e0b4d73db3541c3622db7a3686
ying-css <ying.ye@infineon.com>
2023-09-15 11:35:59 +0800

    1)Updated the test scripts for pubkey saving

--- scripts/curl_nginx_imported_key/curl_nginx_hsm_protected_p12/generate_p12/step2_ECC_client_update_pubkey.sh
+++ scripts/curl_nginx_imported_key/curl_nginx_hsm_protected_p12/generate_p12/step2_ECC_client_update_pubkey.sh
@@ -5,9 +5,9 @@ source config.sh
 set -e
 
 echo "Client1:-----> Extracting public key in PEM and DER from certificate"
-openssl x509 -pubkey -noout -in client1.crt.pem  > client1_pubkey.pem
-openssl x509 -pubkey -noout -in client1.crt.pem | openssl enc -base64 -d > client1_pubkey.der
+openssl x509 -pubkey -noout -in client1_ecc.crt.pem  > client1_ecc_pubkey.pem
+openssl x509 -pubkey -noout -in client1_ecc.crt.pem | openssl enc -base64 -d > client1_ecc_pubkey.der
 
 
 echo "Client1:-----> Update Public key into Optiga Trust M"
-$EXEPATH/trustm_data -w 0xf1d1 -i client1_pubkey.der -e
+$EXEPATH/trustm_data -w 0xf1d1 -i client1_ecc_pubkey.der -e
--- scripts/curl_nginx_imported_key/curl_nginx_hsm_protected_p12/generate_p12/step2_RSA_client_update_pubkey.sh
+++ scripts/curl_nginx_imported_key/curl_nginx_hsm_protected_p12/generate_p12/step2_RSA_client_update_pubkey.sh
@@ -5,9 +5,9 @@ source config.sh
 set -e
 
 echo "Client1:-----> Extracting public key in PEM and DER from certificate"
-openssl x509 -pubkey -noout -in client1.crt.pem  > client1_pubkey.pem
-openssl x509 -pubkey -noout -in client1.crt.pem | openssl enc -base64 -d > client1_pubkey.der
+openssl x509 -pubkey -noout -in client1_rsa.crt.pem  > client1_rsa_pubkey.pem
+openssl x509 -pubkey -noout -in client1_rsa.crt.pem | openssl enc -base64 -d > client1_rsa_pubkey.der
 
 
 echo "Client1:-----> Update Public key into Optiga Trust M"
-$EXEPATH/trustm_data -w $TRUST_M_RSA_PUBKEY_OID -i client1_pubkey.der -e
+$EXEPATH/trustm_data -w 0xf1e0 -i client1_rsa_pubkey.der -e
