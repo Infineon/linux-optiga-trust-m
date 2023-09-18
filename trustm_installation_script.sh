c6bb7be0836380b7d612e8bf9445b0c4bb725227
RaymWong <46432420+RaymWong@users.noreply.github.com>
2023-09-18 21:04:41 +0800

    enable protected update set tool installation by default

--- trustm_installation_script.sh
+++ trustm_installation_script.sh
@@ -6,8 +6,8 @@ CURRENT_DIR="${PWD}"
 PATCH="${PWD}/${FILE}"
 
 
-sudo apt update 
-sudo apt -y install awscli git gcc libssl-dev
+#~ sudo apt update 
+#~ sudo apt -y install awscli git gcc libssl-dev
 
 echo $PATCH
 echo $TRUSTM_LIB_PATH
@@ -20,11 +20,11 @@ make clean
 sudo make uninstall 
 make -j5 
 sudo make install 
-#~ echo "-----> Build Protected Update Set tool"
-#~ cd ex_protected_update_data_set/Linux/
-#~ make clean
-#~ make -j5 
-#~ sudo make install 
+echo "-----> Build Protected Update Set tool"
+cd ex_protected_update_data_set/Linux/
+make clean
+make -j5 
+sudo make install 
  
 #~cd $CURRENT_DIR
 echo "-----> Installation completed. Back to ${PWD}"
