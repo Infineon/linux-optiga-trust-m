059777d16b60c936aaba7fb9edfa9901599ad9b4
RaymWong <46432420+RaymWong@users.noreply.github.com>
2023-09-23 18:13:09 +0800

    Update CURL example with software and HSM protected P12 TLS connection to server

--- scripts/curl_nginx_imported_key/curl_nginx_hsm_protected_p12/step0_reset_rsa_condition.sh
+++ scripts/curl_nginx_imported_key/curl_nginx_hsm_protected_p12/step0_reset_rsa_condition.sh
@@ -6,5 +6,5 @@ RESET="2005d003e1fc07"
 set -e
 
 echo $RESET | xxd -r -p > access_reset.bin
-$EXEPATH/trustm_metadata -w 0xE0FC -F access_reset.bin
-$EXEPATH/trustm_metadata -r 0xE0FC
+$EXEPATH/trustm_metadata -w 0x$TARGET_OID_RSA -F access_reset.bin
+$EXEPATH/trustm_metadata -r 0x$TARGET_OID_RSA
