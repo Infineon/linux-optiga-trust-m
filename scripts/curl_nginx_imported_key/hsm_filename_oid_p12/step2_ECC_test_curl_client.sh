85880974166d481639e5d8ae4f7a0072bd7e3e5d
RaymWong <46432420+RaymWong@users.noreply.github.com>
2023-09-25 21:01:18 +0800

    remove software key P12 example

--- scripts/curl_nginx_imported_key/hsm_filename_oid_p12/step2_ECC_test_curl_client.sh
+++ scripts/curl_nginx_imported_key/hsm_filename_oid_p12/step2_ECC_test_curl_client.sh
@@ -10,8 +10,8 @@ echo "Trust M protected P12 without private key(ECC)"
 sudo cp openssl_curl.cnf /etc/ssl/openssl_curl.cnf
 
 #~ export OPENSSL_CONF=/etc/ssl/openssl_curl.cnf  
-echo " Using software P12 file"
-curl -v --cert-type P12  --cert client1.p12:1234 --cacert $CA_CERT https://127.0.0.1
+#~ echo " Using software P12 file"
+#~ curl -v --cert-type P12  --cert client1.p12:1234 --cacert $CA_CERT https://127.0.0.1
 
 echo " Using HSM protected P12 file"
 OPENSSL_CONF=/etc/ssl/openssl_curl.cnf curl -v --engine trustm_engine --cert-type ENG  --cert e0f1.p12 --cacert $CA_CERT https://127.0.0.1
