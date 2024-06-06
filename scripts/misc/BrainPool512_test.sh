67f320ab1789b6fd8ae392a456176d3f293d9340
ying-css <ying.ye@infineon.com>
2024-06-06 13:29:15 +0800

    1)updated test scripts for ECC256/384/521 and Brainpool256/384/512

--- scripts/misc/BrainPool512_test.sh
+++ scripts/misc/BrainPool512_test.sh
@@ -15,17 +15,17 @@ set -e
 
 echo "Testing Brainpool512"
 echo "Trust M key gen for Brainpool512 at 0x$KEY_OID"
-#~ $EXEPATH/trustm_ecc_keygen -g 0x$KEY_OID -t 0x13 -k 0x16 -o test_pub_$KEY_OID.pem -s
-$EXEPATH/trustm_ecc_keygen -g 0x$KEY_OID -t 0x13 -k 0x16 -o test_pub_$KEY_OID.pem 
+$EXEPATH/trustm_ecc_keygen -g 0x$KEY_OID -t 0x13 -k 0x16 -o test_pub_$KEY_OID.pem -s
+#~ $EXEPATH/trustm_ecc_keygen -g 0x$KEY_OID -t 0x13 -k 0x16 -o test_pub_$KEY_OID.pem 
 
 echo "Brain Pool Signature512 by TrustM:"
-$EXEPATH/trustm_ecc_sign -k 0x$KEY_OID -o testsignature_BP512.bin -i mydata.txt -H 
+$EXEPATH/trustm_ecc_sign -k 0x$KEY_OID -o testsignature_BP512.bin -i mydata.txt -H sha512
 xxd testsignature_BP512.bin
 
 echo "Verify Brain Pool Signature512 by TrustM:"
-$EXEPATH/trustm_ecc_verify -i mydata.txt -s testsignature_BP512.bin -p test_pub_$KEY_OID.pem -H
+$EXEPATH/trustm_ecc_verify -i mydata.txt -s testsignature_BP512.bin -p test_pub_$KEY_OID.pem -H sha512
 echo "verify with openssl sign by TrustM"
-openssl dgst -verify test_pub_$KEY_OID.pem -keyform pem -sha256 -signature testsignature_BP512.bin mydata.txt
+openssl dgst -verify test_pub_$KEY_OID.pem -keyform pem -sha512 -signature testsignature_BP512.bin mydata.txt
 
 done
 sleep 1
