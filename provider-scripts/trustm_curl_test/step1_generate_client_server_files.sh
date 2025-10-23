fa35160375f9f1fc0909f57bd8e4574f41fec3cb
ying-css <ying.ye@infineon.com>
2025-10-23 14:50:25 +0800

    1)updated the test scripts for curl

--- provider-scripts/trustm_curl_test/step1_generate_client_server_files.sh
+++ provider-scripts/trustm_curl_test/step1_generate_client_server_files.sh
@@ -13,4 +13,5 @@ openssl x509 -in ${SERVER_FILE}.crt -text -noout
 echo "[*] Client1:"
 openssl pkey -provider trustm_provider -provider default -propquery provider=trustm -in 0xe0f1:*:NEW:0x03:0x13 -out ${CLIENT_FILE}.key
 openssl req -new -provider trustm_provider -provider default -key ${CLIENT_FILE}.key -out ${CLIENT_FILE}.csr -subj "/C=SG/CN=TrustM"
-openssl x509 -req -in ${CLIENT_FILE}.csr -CA $CERT_PATH/ca.cert.pem -CAkey $CERT_PATH/ca.key -CAcreateserial -out ${CLIENT_FILE}.crt -days 365 -sha256 #-extfile cert_ext.cnf -extensions cert_ext
+openssl x509 -req -in ${CLIENT_FILE}.csr -CA $CERT_PATH/ca.cert.pem -CAkey $CERT_PATH/ca.key -CAcreateserial -out ${CLIENT_FILE}.crt -days 365 -sha256 -extfile cert_ext.cnf -extensions cert_ext
+openssl x509 -in ${CLIENT_FILE}.crt -text -noout
