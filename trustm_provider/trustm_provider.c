cc097f28fd43bd5a21bda5ac3800cc198e9b43f6
ying-css <ying.ye@infineon.com>
2026-01-15 13:55:04 +0800

    1)ECC works for tls1.2 when using curl for server client communication

--- trustm_provider/trustm_provider.c
+++ trustm_provider/trustm_provider.c
@@ -11,6 +11,7 @@
 #include <openssl/core_names.h>
 #include <openssl/params.h>
 #include <openssl/crypto.h>
+#include <openssl/prov_ssl.h>
 
 
 #include <sys/ipc.h>
@@ -436,6 +437,7 @@ static const OSSL_ALGORITHM trustm_asymciphers[] = {
     { "RSA:rsaEncryption", "provider=trustm", trustm_rsa_asymcipher_functions },
     { NULL, NULL, NULL }
 };
+#define NELEMS(x)  (sizeof(x) / sizeof((x)[0]))
 
 static const OSSL_ALGORITHM *trustm_query_operation(void *provctx, int operation_id, int *no_cache) 
 {
@@ -524,6 +526,71 @@ static void trustm_teardown(void *provctx)
     OPENSSL_clear_free(trustm_ctx, sizeof(trustm_ctx_t));
 }
 
+typedef struct tls_group_constants_st {
+    unsigned int group_id;   /* Group ID */
+    unsigned int secbits;    /* Bits of security */
+    int mintls;              /* Minimum TLS version, -1 unsupported */
+    int maxtls;              /* Maximum TLS version (or 0 for undefined) */
+    int mindtls;             /* Minimum DTLS version, -1 unsupported */
+    int maxdtls;             /* Maximum DTLS version (or 0 for undefined) */
+} TLS_GROUP_CONSTANTS;
+
+#define TLS_GROUP_ID_secp256r1 23
+#define TLS_GROUP_ID_secp384r1 24
+#define TLS_GROUP_ID_secp521r1 25
+
+static const TLS_GROUP_CONSTANTS tls_group_list[] = {
+    { TLS_GROUP_ID_secp256r1, 128, TLS1_VERSION, 0, DTLS1_VERSION, 0 },
+    { TLS_GROUP_ID_secp384r1, 192, TLS1_VERSION, 0, DTLS1_VERSION, 0 },
+    { TLS_GROUP_ID_secp521r1, 256, TLS1_VERSION, 0, DTLS1_VERSION, 0 },
+};
+#define TLS_GROUP_ENTRY(tlsname, realname, algorithm, idx) \
+    { \
+        OSSL_PARAM_utf8_string(OSSL_CAPABILITY_TLS_GROUP_NAME, \
+                               tlsname, \
+                               sizeof(tlsname)), \
+        OSSL_PARAM_utf8_string(OSSL_CAPABILITY_TLS_GROUP_NAME_INTERNAL, \
+                               realname, \
+                               sizeof(realname)), \
+        OSSL_PARAM_utf8_string(OSSL_CAPABILITY_TLS_GROUP_ALG, \
+                               algorithm, \
+                               sizeof(algorithm)), \
+        OSSL_PARAM_uint(OSSL_CAPABILITY_TLS_GROUP_ID, \
+                        (unsigned int *)&tls_group_list[idx].group_id), \
+        OSSL_PARAM_uint(OSSL_CAPABILITY_TLS_GROUP_SECURITY_BITS, \
+                        (unsigned int *)&tls_group_list[idx].secbits), \
+        OSSL_PARAM_int(OSSL_CAPABILITY_TLS_GROUP_MIN_TLS, \
+                       (int *)&tls_group_list[idx].mintls),     \
+        OSSL_PARAM_int(OSSL_CAPABILITY_TLS_GROUP_MAX_TLS, \
+                       (int *)&tls_group_list[idx].maxtls), \
+        OSSL_PARAM_int(OSSL_CAPABILITY_TLS_GROUP_MIN_DTLS, \
+                       (int *)&tls_group_list[idx].mindtls), \
+        OSSL_PARAM_int(OSSL_CAPABILITY_TLS_GROUP_MAX_DTLS, \
+                       (int *)&tls_group_list[idx].maxdtls), \
+        OSSL_PARAM_END \
+    }
+    
+static const OSSL_PARAM param_tls_group_list[][10] = {
+    TLS_GROUP_ENTRY("secp256r1", "prime256v1", "EC", 0),
+    TLS_GROUP_ENTRY("P-256",     "prime256v1", "EC", 0),
+    TLS_GROUP_ENTRY("secp384r1", "secp384r1", "EC", 1),
+    TLS_GROUP_ENTRY("P-384", "secp384r1", "EC", 1), 
+    TLS_GROUP_ENTRY("secp521r1", "secp521r1", "EC", 2),
+    TLS_GROUP_ENTRY("P-521", "secp521r1", "EC", 2), 
+};
+
+static int trustm_get_capabilities(void *provctx, const char *capability,
+                                   OSSL_CALLBACK *cb, void *arg)
+{
+    if (OPENSSL_strcasecmp(capability, "TLS-GROUP") == 0) {
+        size_t i;
+        for (i = 0; i < NELEMS(param_tls_group_list); i++)
+            if (!cb(param_tls_group_list[i], arg))
+                return 0;
+        return 1;
+    }
+    return 0;
+}
 // todo: expand this list as more functions get implemented
 static const OSSL_ITEM * trustm_get_reason_strings(void *provctx) 
 {
@@ -544,6 +611,7 @@ static const OSSL_DISPATCH trustm_dispatch_table[] = {
     { OSSL_FUNC_PROVIDER_QUERY_OPERATION, (void (*) (void))trustm_query_operation },
     { OSSL_FUNC_PROVIDER_TEARDOWN, (void (*)(void)) trustm_teardown },
     { OSSL_FUNC_PROVIDER_GET_REASON_STRINGS, (void (*)(void)) trustm_get_reason_strings},
+    { OSSL_FUNC_PROVIDER_GET_CAPABILITIES, (void (*)(void))trustm_get_capabilities},
     { 0, NULL }
 };
 
