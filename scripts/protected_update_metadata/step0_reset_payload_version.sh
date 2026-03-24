1c7748300aabeec934921f7295b87457ac7c8030
ying-css <ying.ye@infineon.com>
2026-03-24 10:07:19 +0800

    1)fixed the test script for metadata protected update

--- scripts/protected_update_metadata/step0_reset_payload_version.sh
+++ scripts/protected_update_metadata/step0_reset_payload_version.sh
@@ -5,11 +5,11 @@ set -e
  
 RESET_PAYLOAD_VER_META="2004C1020000"
 
-echo "Reset payload version for 0x$TARGET_ECC_OID"
+echo "Reset payload version for 0x$TARGET_OID"
 echo $RESET_PAYLOAD_VER_META | xxd -r -p > reset_payload_ver_metadata.bin
 echo "Printout reset_payload_ver_metadata.bin"
 xxd reset_payload_ver_metadata.bin
-echo "Write reset_payload_ver_metadata.bin as metadata of 0x$TARGET_ECC_OID"
-$EXEPATH/trustm_metadata -w 0x$TARGET_ECC_OID -F reset_payload_ver_metadata.bin
-echo "Read out metadata for 0x$TARGET_ECC_OID after reset"
-$EXEPATH/trustm_metadata -r  0x$TARGET_ECC_OID
+echo "Write reset_payload_ver_metadata.bin as metadata of 0x$TARGET_OID"
+$EXEPATH/trustm_metadata -w 0x$TARGET_OID -F reset_payload_ver_metadata.bin
+echo "Read out metadata for 0x$TARGET_OID after reset"
+$EXEPATH/trustm_metadata -r  0x$TARGET_OID
